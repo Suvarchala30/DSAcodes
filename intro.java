50b53fd9c1287fe7a4f2db7f7a35799943137ea3
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-03-25 21:03:37 +0530

    dsastart

---
+++ intro.java
@@ -0,0 +1,20 @@
+public class intro{
+    public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+
+        int x = 25;
+        int y =10;
+
+        System.out.println(((float)x/y));
+
+        int a=20;
+        int b=15;
+        float z=a/b;
+        System.out.println(z); //returns 1.0 not 1.2
+        //char datatype value should always be in single quotes
+        char mychar='w';//char only has single character
+        System.out.println(mychar);
+        String str = "suvarchala";
+        System.out.println(str);
+	}
+}
